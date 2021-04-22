189d0cd8e010cd6db1cf28bfe1e73dc0f3db2905
Nathan Kent <ngkent75@gmail.com>
2021-04-21 21:14:13 -0400

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const Url = 'https://randomuser.me/api/?results=30';
-
-const search = {
-    getAllUsers: () => axios.get(Url).then(({ data }) => data.results)
-};
-
-export default search;
