aefbf47f3fd45b5e331b6dfd4a5c2f1b8c1295df
Nate Kent <ngkent75@gmail.com>
2021-04-21 20:34:46 -0400

    added table component where employees and their data will be organized

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -4,10 +4,10 @@ function Navbar() {
     return (
         <nav class="navbar navbar-light bg-light justify-content-between">
             <h1 class="navbar-brand">Employee Directory</h1>
-            <form class="form-inline">
+            {/* <form class="form-inline">
                 <input> class="form-control mr-sm-2" type="search" placeholder="Search for Employee Name" aria-label="Search</input>
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            </form> */}
         </nav>
     )
 };
---
+++ src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { Container, Row, Form, InputGroup } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
+import EmTable from '../Table/Table';
+
+const Main = () => {
+    const [searchEm, setSearchEm] = useState('';)
+}
